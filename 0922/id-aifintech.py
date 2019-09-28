24cec18fb885be8433d6252de51b3a0c456742a7
xiangzhsy@163.com <xzhsy2016>
2019-09-28 19:59:03 +0800

    新增网站无法登陆 抓去cookie 登陆

--- 0922/id-aifintech.py
+++ 0922/id-aifintech.py
@@ -8,18 +8,27 @@ from http import cookiejar
 socket.setdefaulttimeout(20)
 
 class Connect(object):
-    def __init__(self):
+    def __init__(self,cookie):
         self.headers = {
-            "Host": " adminweb.adminrichboxbox.com",
-            "User-Agent": " Mozilla/5.0 (X11; Linux x86_64; rv:60.0) Gecko/20100101 Firefox/60.0",
-            "Accept": "*/*",
-            "Accept-Language": " zh-CN,zh;q=0.8,zh-TW;q=0.7,zh-HK;q=0.5,en-US;q=0.3,en;q=0.2",
-            "Accept-Encoding": " gzip, deflate, br",
-            "Referer": "https://adminweb.adminrichboxbox.com/collection/my",
-            "x-admin-token": " eyJsb2NhbGUiOiJ6aF9DTiIsImFsZyI6IkhTNTEyIn0.eyJzdWIiOiIwMDkiLCJleHAiOjE1NjkwNDI3MDh9.B9Q6ZGi8b05eYJBwSLNWW19q70zTpeizKLe8c3B_xsHi6RO1GEAfbvyBAfYybU4Z30irzlbjYsW002AO4Q9ScA",
-            "origin": " https://adminweb.adminrichboxbox.com",
-            "Connection": "keep-alive"
+            "authority": " lmt-man.id-aifintech.cc",
+            "method": " POST",
+            "path": " /cs/collection/getTotalList?flag=member",
+            "scheme": " https",
+            "accept": " application/json, text/javascript, */*; q=0.01",
+            "accept-encoding": " gzip, deflate, br",
+            "accept-language": " zh-CN,zh;q=0.9",
+            "cache-control": " no-cache",
+            "content-length": " 14",
+            "content-type": " application/x-www-form-urlencoded; charset=UTF-8",
+            "origin": " https://lmt-man.id-aifintech.cc",
+            "pragma": " no-cache",
+            "referer": " https://lmt-man.id-aifintech.cc/cs/common/frame/index?jspname=pages/csm/addressList/collectionCollector_query",
+            "sec-fetch-mode": " cors",
+            "sec-fetch-site": " same-origin",
+            "user-agent": " Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/76.0.3809.100 Safari/537.36",
+            "x-requested-with": " XMLHttpRequest"
         }
+        self.cookie=cookie
         #自定义请求方法，获取cookie
         cookie_object = cookiejar.CookieJar()
         hanler = urllib.request.HTTPCookieProcessor(cookie_object)
@@ -27,49 +36,57 @@ class Connect(object):
 
         #定义请求连接
         self.baseurl='https://lmt-man.id-aifintech.cc/cs/collection/getTotalList?flag=member'
-        self.userUrl='https://adminweb.adminrichboxbox.com/api/api/review/personalInfo?'
-        self.loadUrl='https://adminweb.adminrichboxbox.com/api/api/collection/collection-loan-detail?'
         #登陆连接
+        self.before='https://lmt-man.id-aifintech.cc/cs/login'
         self.login = 'https://lmt-man.id-aifintech.cc/cs/j_spring_security_check'
 
     def loginaction(self,userdataf):
-        userdata = urllib.parse.urlencode(userdataf).encode(encoding='UTF8')
-        login = urllib.request.Request(self.login, method='POST', data=userdata, headers=self.headers)
-        response = self.opener.open(login)
-        response.close()
+        try:
+            before = urllib.request.Request(self.before)
+            beforeponse = self.opener.open(before)
+            userdata = urllib.parse.urlencode(userdataf).encode(encoding='UTF8')
+            login = urllib.request.Request(self.login, method='POST', data=userdata, headers=self.headers)
+            response = self.opener.open(login)
+            response.close()
 
-    def reqbase(self,nexta,limit,status):
-        offset=nexta
+        except Exception as e:
+            print("登陆失败：", e)
+            exit(1)
+
+    def reqbase(self,page,limit):
         limit=limit
         dataformat = {
-            "offset": offset,
-            "collectionTaskStatus": status,
-            "limit": limit
+            "page": page,
+            "rows": limit
         }
-        data = urllib.parse.urlencode(dataformat)
-        baseurl=self.baseurl+data
-        request = urllib.request.Request(url=baseurl, method='GET',
+        self.headers['Cookie'] = "lang=zh; JSESSIONID=" + self.cookie + "; INST_ORG=bj_qianbang"
+        data = urllib.parse.urlencode(dataformat).encode(encoding='UTF8')
+        baseurl=self.baseurl
+        request = urllib.request.Request(url=baseurl, method='POST',data=data,
                                          headers=self.headers)
         userinfo=[]
         infodic={}
         print(request.full_url)
-        response = self.opener.open(request)
-        # print(data)
-        contentb = str(response.read(), encoding='utf-8')
+        # response = self.opener.open(request)
+        response = urllib.request.urlopen(request)
+        print(data)
+        contentb = str(response.read(), encoding='utf-8').strip("'")
         # print(contentb)
         content = json.loads(contentb, strict=False)
         response.close()
-        infodic['totalCount'] = content['paginator']['totalCount']
-        infodic['offset'] = content['paginator']['offset']
-        for i in  content['item']:
+        infodic['totalCount'] = content['total']
+        for i in  content['rows']:
             midinfo = {}
             midinfo['入催时间'] = i['createTime']
             midinfo['客户名称'] = i['chname']
             midinfo['手机号码'] = i['mobile']
-            midinfo['出崔时间'] = i['outDate']
+            if 'outDate' in i:
+                midinfo['出崔时间'] = i['outDate']
+            else:
+                midinfo['出崔时间'] =''
             midinfo['应还日期'] = i['dueDate']
             midinfo['最大逾期天数'] = i['hisMaxOverdueDays']
-            midinfo['任务状态'] = i['collectionTaskStatus']['value']
+            midinfo['业务状态'] = i['states']
             midinfo['身份证号'] = i['idNumber']
             userinfo.append(midinfo)
         with open(filename, 'a',newline='') as f:
@@ -85,8 +102,8 @@ class Connect(object):
 if __name__ == '__main__':
     now = datetime.datetime.now()
     format = "%Y-%m-%d-%H-%M-%S"
-
-    basereq = Connect()
+    cookie=sys.argv[3]
+    basereq = Connect(cookie)
     userdataf = {}
     if len(sys.argv) >= 2:
         userdataf['j_username'] = sys.argv[1]
@@ -95,7 +112,7 @@ if __name__ == '__main__':
         userdataf['j_username'] = input('输入用户名： ')
         userdataf['j_password'] = input('输入密码： ')
     basereq.loginaction(userdataf)
-    filename=userdataf['mobile']+ '-' + now.strftime(format) + '.csv'
+    filename=userdataf['j_username']+ '-' + now.strftime(format) + '.csv'
     with open(filename, 'w') as f:
         head = ['客户名称', '手机号码', '入催时间', '出崔时间', '应还日期', '最大逾期天数', '身份证号', '业务状态'
                 ]
@@ -103,11 +120,11 @@ if __name__ == '__main__':
         writer = csv.DictWriter(f, head)
         writer.writeheader()
     # basereq.headers['Cookie'] = sys.argv[1]
-    args=['PROCESSED','PROCESS_FAILED','CANCELED']
-    for i in args:
-        baseinfo = basereq.reqbase(0, 10,i)
-        offset = baseinfo['offset'] + 10
-        totalCount = baseinfo['totalCount']
-        while offset <= totalCount:
-            baseinfo = basereq.reqbase(offset, 100,i)
-            offset = offset + 100
+    baseinfo = basereq.reqbase(1, 50)
+    totalCount = baseinfo['totalCount']
+    page=2
+    limit=50
+    pages=round(totalCount/limit)
+    while page <= pages:
+        baseinfo = basereq.reqbase(page, limit)
+        page = page + 1
